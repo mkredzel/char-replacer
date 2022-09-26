e45a9b7081f509ff4ab3ec0f7926ca8255f8ed97
Marcel Kredzel <marcel.kredzel@gmail.com>
2022-09-27 00:00:44 +0100

    Fix removing replacers, formatting, jQuery refactor

--- main.js
+++ main.js
@@ -1,73 +1,77 @@
 let replaces = [];
 
-let addItem = document.querySelector("#addItem");
-let transformText = document.querySelector("#transformText");
-let message = document.querySelector("#message");
+let addItem = $("#addItem");
+let transformText = $("#transformText");
+let message = $("#message");
 
-let trash = document.querySelector("#delete");
+let trash = $("#delete");
 
 // click event for add button
-transformText.addEventListener("click", function () {
-  let text = document.querySelector("#text").value;
+transformText.on("click", function () {
+  let text = $("#text").val();
+
+  if (!text) {
+    message.html("<br>Enter text...");
+    return;
+  }
 
   let output;
 
   if (replaces.length > 0) {
     replaces.forEach((el) => {
-      output = text.replaceAll(el.replaced, el.replacer);
+      if (el.replaced != 0 && el.replacer != 0) {
+        output = text.replaceAll(el.replaced, el.replacer);
+      }
     });
   } else {
     output = text;
   }
 
-  document.querySelector("#output").innerHTML = output;
-  document.querySelector("#output-container").style.display = "block";
+  $("#output").html(output);
+  $("#output-container").show();
 });
 
 // click event for add button
-addItem.addEventListener(
-  "click",
-  function () {
-    // get value of user input
-    let replaced = document.querySelector("#replaced-item").value;
-    let replacer = document.querySelector("#replacer-item").value;
+addItem.on("click", function () {
+  // get value of user input
+  let replaced = $("#replaced-item").val();
+  let replacer = $("#replacer-item").val();
 
-    if (replaced == "") {
-      message.innerHTML = "<br>Enter a task.";
-    } else {
-      // remove message
-      message.innerHTML = "";
+  if (!replaced || !replacer) {
+    message.html("<br>Enter values...");
+  } else {
+    // remove message
+    message.html("");
 
-      // reset input field
-      document.querySelector("#replaced-item").value = "";
-      document.querySelector("#replacer-item").value = "";
+    // reset input field
+    $("#replaced-item").val("");
+    $("#replacer-item").val("");
 
-      replaces.push({
-        replaced: replaced,
-        replacer: replacer,
-      });
+    replaces.push({
+      replaced: replaced,
+      replacer: replacer,
+    });
 
-      // creates new li element
-      let itemAdded = document.createElement("li");
-      let deleteitem = `${replaced}-${replacer}`.replaceAll(' ', '');
-      itemAdded.innerHTML = '<span id=' + deleteitem + '>Replace <b>' + replaced + '</b> with <b>' + replacer + '</b> <i class="fa-solid fa-trash" onclick="DeleteMe(`' + deleteitem + '`)"></i></span>';
-      itemAdded.className = "added";
-      itemAdded.setAttribute('id', JSON.stringify(`${replaced}-${replacer}`));
+    let idx = replaces.length - 1;
 
-      let mainList = document.querySelector(".myList");
-      mainList.appendChild(itemAdded);
-    }
-  },
-  false
-);
+    $("<li>")
+      .html(
+        `<span id='id${idx}'>Replace <b>` +
+          replaced +
+          `</b> with <b>` +
+          replacer +
+          `</b> <i class="fa-solid fa-trash" onclick="DeleteMe('${idx}')"></i></span>`
+      )
+      .addClass("added")
+      .appendTo(".myList");
+  }
+});
 
-function DeleteMe(item) {
+function DeleteMe(i) {
+  console.log(`#id${i}`);
+  console.log(replaces);
 
-  document.querySelector(
-    `#${item}`
-  ).parentNode.style.display = "none";
+  $(`#id${i}`).parent().hide();
 
-  replaces = replaces.filter(function (el) {
-    return `${el.replaced}-${el.replacer}`.replaceAll(' ', '') != JSON.parse(item);
-  });
+  replaces[i] = 0;
 }
